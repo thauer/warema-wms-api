cdb013dfe82e2fc7f9855538b53ad5b7cc632c96
thauer <1329526+thauer@users.noreply.github.com>
2023-02-18 20:57:21 +0000

    Simplified README, published 1.0.1

---
+++ test/wms.js
@@ -0,0 +1,100 @@
+const WmsVbStick = require("warema-wms-api");
+weather = {}
+
+function callback( err, msg ){
+    console.log("hello");
+    if( err ){ 
+      console.log( "Callback err: " + err ); 
+      return;
+    }
+    switch( msg.topic ) {
+      case "wms-vb-init-completion" : 
+        // Enable callback messages for changes of position 
+        stick.setPosUpdInterval( 5000 );
+        break;
+      case "wms-vb-rcv-weather-broadcast":
+        w = msg.payload.weather
+        if(false) {
+          console.log(`${w.snr}: ${w.lumen.toString().padStart(8)}lm `+
+                      `${w.temp.toFixed(1)}°C ${w.wind} rain: ${w.rain ? "yes" : "no"}`)
+        }
+  
+        side = {1279382: "12793(82)", 1279335: "12793(35)", 1279387: "12793(87)", 1279393: "West(93)"}
+        weather[side[w.snr]] = {'lumen': w.lumen, 'temp': w.temp, 'wind': w.wind, 'rain': w.rain}
+  
+        break;
+      case "wms-vb-blind-position-update":
+        p = msg.payload
+        console.log(`Callback: ${p.name}: `
+          + `pos:${p.position} ang:${p.angle} moving: ${p.moving}`)
+        break
+      case "wms-vb-scanned-devices":
+      case "wms-vb-cmd-result-set-position":
+      case "wms-vb-cmd-result-stop":
+      default:
+        console.log( "Callback msg: " + JSON.stringify( msg ) );
+    }
+}
+
+
+console.log( "starting ..." );
+
+var wms_channel = 17
+var wms_panid = "D2BD"
+var wms_key = "1F29066087C70C9A9EF592F6A21444BE"
+
+stick = new WmsVbStick( "/dev/ttyUSB0", wms_channel, wms_panid, wms_key, {}, callback );
+
+console.log( "finished." );
+
+stick.vnBlindAdd(1260043,"Ben"    )
+stick.vnBlindAdd(1187205,"Lilla"  )
+stick.vnBlindAdd(1190506,"Bedroom")
+stick.vnBlindAdd(1190504,"E1" )
+stick.vnBlindAdd(1190503,"E2" )
+stick.vnBlindAdd(1260229,"S1")
+stick.vnBlindAdd(1259963,"S2")
+stick.vnBlindAdd(1268208,"S3")
+stick.vnBlindAdd( 883045,"S4")
+stick.vnBlindAdd(1190496,"S5" )
+stick.vnBlindAdd(1190454,"S6" )
+stick.vnBlindAdd(1259545,"W1")
+
+stick.vnBlindAdd(1247705, "Awning");
+stick.vnBlindAdd(1247909, "Awning West");
+
+
+blinds = stick.vnBlindsList()
+
+livingroom = ["E1", "E2", "S1", "S2", "S3", "S4", "S5", "S6", "W1"]
+east = ["E1", "E2", "Ben", "Lilla"]
+west = ["W1", "S5", "Bedroom"]
+south = ["S1", "S2", "S3", "S4", "S6"]
+
+// stick.vnBlindGetPosition("E1")
+// stick.vnBlindSetPosition("W1", 100, 0)
+
+/* position: percent from 0 to 100. 
+       At 0 (open) set angle to -100 to retract into the cover.
+angle: -100: the slats are completely inclined inwards. 
+          0: the slats are in horizontal position. 
+        100: the slats are completely inclined outwards.
+*/
+function setBlinds(blinds, position, angle ) {
+  for (const blind of blinds) {
+    stick.vnBlindSetPosition(blind, position, angle)
+  }
+}
+
+function lower( blinds ) {
+  setBlinds(blinds, 100, 0)
+}
+
+function raise( blinds ) {
+  setBlinds(blinds, 0, -100)
+}
+
+module.exports = { lower, raise };
+
+raise(west)
+//raise(south)
