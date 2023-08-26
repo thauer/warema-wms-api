4c328232f2d1e6f82bcfdca507edb4df3e9f4b3f
thauer <1329526+thauer@users.noreply.github.com>
2023-08-26 18:52:39 +0000

    Testing all init commands

--- wms/stick.spec.js
+++ wms/stick.spec.js
@@ -1,9 +1,11 @@
 
 const { SerialPort } = require('serialport');
+const { DelimiterParser }  = require('@serialport/parser-delimiter')
+const DelimiterChar = '}';
 
 const WmsVbStick = require("../../warema-wms-api");
 
-test.only("Verify that testing works", () => {
+test("Verify that testing works", () => {
     expect("hello").toBe("hello");
 });
 
@@ -15,26 +17,93 @@ test("SerialPort should be available", () => {
     })
 })
 
-test.only("SerialPort open and close", () => {
+test("SerialPort can be opened and closed", () => {
     port = new SerialPort( { path: "/dev/ttyUSB0" , baudRate: 125000 })
     port.on('open', ()=>{ expect(port.isOpen)})
     setTimeout( () => { 
         expect(port.isOpen);
         port.close();
         expect(!port.isOpen);
-    }, 200 );
+    }, 100 );
 })
 
-test("stick", () => {
-    expect(1).toBe(1);
-    console.log("hi");
+test.skip("Command {G} returns stick name", done => {
+    port = new SerialPort( { path: "/dev/ttyUSB0" , baudRate: 125000 });
+    parser = port.pipe(new DelimiterParser({ delimiter: '}' }));
 
-    function callback( err, msg ){ }
-    
-    var wms_channel = 17
-    var wms_panid = "D2BD"
-    var wms_key = "1F29066087C70C9A9EF592F6A21444BE"
-    
-    stick = new WmsVbStick( "/dev/ttyUSB0", wms_channel, wms_panid, wms_key, {}, callback );
+    received = ""
 
+    parser.on('data', function (data) {
+        received = received + data.toString('utf8') + '}';
+    });
+
+    port.on('open', () => { 
+        port.write("{G}");
+    })
+
+    setTimeout( () => {
+        expect(received).toMatch(/{gWMS USB-Stick}/);
+        port.close();
+        done();
+    }, 200 );
+});
+
+test.skip("Command {V} returns stick version", done => {
+    port = new SerialPort( { path: "/dev/ttyUSB0" , baudRate: 125000 });
+    parser = port.pipe(new DelimiterParser({ delimiter: '}' }));
+
+    received = ""
+    parser.on('data', function (data) {
+        received = received + data.toString('utf8') + '}';
+    });
+
+    port.on('open', () => { 
+        port.write("{V}");
+    })
+
+    setTimeout( () => {
+        expect(received).toMatch(/{v37605107   }/);
+        port.close();
+        done();
+    }, 200 );
+});
+
+test.skip("Command {K} sets stick key and receives ACK", done => {
+    port = new SerialPort( { path: "/dev/ttyUSB0" , baudRate: 125000 });
+    parser = port.pipe(new DelimiterParser({ delimiter: '}' }));
+
+    received = ""
+    parser.on('data', function (data) {
+        received = received + data.toString('utf8') + '}';
+    });
+
+    port.on('open', () => { 
+        port.write('{K4011F29066087C70C9A9EF592F6A21444BE}');
+    })
+
+    setTimeout( () => {
+        expect(received).toMatch(/{a}/);
+        port.close();
+        done();
+    }, 200 );
+});
+
+test.only("Command {M} sets stick channel and receives ACK", done => {
+    port = new SerialPort( { path: "/dev/ttyUSB0" , baudRate: 125000 });
+    parser = port.pipe(new DelimiterParser({ delimiter: '}' }));
+
+    received = ""
+    parser.on('data', function (data) {
+        received = received + data.toString('utf8') + '}';
+    });
+
+    port.on('open', () => { 
+        port.write("{M%" + "17" + "D2BD" + "}");
+    })
+
+    setTimeout( () => {
+        expect(received).toMatch(/{a}/);
+        port.close();
+        done();
+    }, 200 );
 });
